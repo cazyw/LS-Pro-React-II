45c5ab45e20d667e398cfdaa54b94faae9163f68
cazyw <kazeisc@gmail.com>
2017-09-25 22:55:42 +1000

    Test changing minutes calculation

--- clock/src/Clock.js
+++ clock/src/Clock.js
@@ -64,7 +64,7 @@ export default class Clock extends Component {
       <div className='panel panel-default'>
         <div className='panel-heading'>
           <div className='panel-title'>
-            console.log('{this.state.clock.getMinutes()}')
+            console.log(console {this.state.clock.getMinutes()}')
             The local time is currently 
           </div>
         </div>
